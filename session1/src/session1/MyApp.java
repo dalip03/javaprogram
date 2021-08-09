fef82380c25985d5757c1cf770f797394b526f43
dalipdivaker <dalipdivakar@gmail.com>
2021-08-09 20:36:04 +0530

    Introduction To Java

---
+++ session1/src/session1/MyApp.java
@@ -0,0 +1,19 @@
+package session1;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello All From Eclipse");
+		System.out.println("Be exceptional");
+		System.out.println("Code with heart and brain together :)");
+		
+int age = 10;
+age = 20;
+
+System.out.println("age is : "+age);
+double amount = 10000.45;
+System.out.println("Amout is : "+amount);
+
+	}
+
+}
