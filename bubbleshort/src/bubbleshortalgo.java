1c9bc9382bd19756f8d374a61fcca252e1796bb0
dalipdivaker <dalipdivakar@gmail.com>
2021-08-09 20:34:21 +0530

    Bubble short

---
+++ bubbleshort/src/bubbleshortalgo.java
@@ -0,0 +1,21 @@
+
+public class bubbleshortalgo {
+
+	public static void main(String[] args) {
+
+		int[] data = {11, 5, 20, 8, 17, 12};
+		
+		for(int i=0;i<data.length;i++) {
+		System.out.println("For i: "+i);
+		System.out.println("j is: ");
+		for(int j=0;j<data.length - i - 1; j++) {
+			System.out.println(j+" ");
+		}
+			
+		System.out.println();
+		}
+		
+		
+		
+	}
+}
